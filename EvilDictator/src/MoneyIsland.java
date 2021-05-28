5cc02e44dc0a6da195e9acf3cbd08dd8ba32d510
Petko-Ivanov <82137543+Petko-Ivanov@users.noreply.github.com>
2021-05-28 14:46:02 +0300

    Evil Dictator

---
+++ EvilDictator/src/MoneyIsland.java
@@ -0,0 +1,7 @@
+public class MoneyIsland {
+    public static final String ISLAND_ICON=" $ ";
+    public int islandCoordinatesRow=0;
+    public int islandCoordinatesCol=0;
+    public boolean isTradingAvailable=true;
+
+}
